02ef4b5b2741f29b015fbf9267afa7655b644f79
neonowy01 <neonowy@int.pl>
2024-07-22 03:00:23 +0200

    Post-game screen

--- panorama/scripts/custom_game/buttons.js
+++ panorama/scripts/custom_game/buttons.js
@@ -7,3 +7,15 @@ function Clicked()
 {
      $.DispatchEvent("ClientUI_FireOutput", 1);
 }
+
+function Exit()
+{
+     $.DispatchEvent("ClientUI_FireOutput", 1);
+     $.DispatchEvent("ClientUI_FireOutput", 2);
+}
+
+function Restart()
+{
+     $.DispatchEvent("ClientUI_FireOutput", 1);
+     $.DispatchEvent("ClientUI_FireOutput", 3);
+}
